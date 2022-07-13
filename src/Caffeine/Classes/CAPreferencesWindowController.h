e64537d9a9f7d420c0f458152da9fac3ef4cb134
Dominic Rodemer <dominic.rodemer@me.com>
2022-07-13 10:55:42 +0200

    Implemented dynamic layout for localizations

--- src/Caffeine/Classes/CAPreferencesWindowController.h
+++ src/Caffeine/Classes/CAPreferencesWindowController.h
@@ -9,15 +9,35 @@
 
 @interface CAPreferencesWindowController : NSWindowController
 {
+    NSImageView *iconView;
+    
+    NSTextField *informationTextField;
+    NSTextField *instructionsTextField;
+    
+    NSTextField *durationsTextField;
     NSPopUpButton *durationButton;
+    
     NSButton *activateAtLaunchButton;
     NSButton *deactivateAtOnManualSleepButton;
     NSButton *showAtLaunchButton;
+    
+    NSButton *quitButton;
+    NSButton *closeButton;
 }
 
+@property (nonatomic, strong) IBOutlet NSImageView *iconView;
+
+@property (nonatomic, strong) IBOutlet NSTextField *informationTextField;
+@property (nonatomic, strong) IBOutlet NSTextField *instructionsTextField;
+
+@property (nonatomic, strong) IBOutlet NSTextField *durationsTextField;
 @property (nonatomic, strong) IBOutlet NSPopUpButton *durationButton;
+
 @property (nonatomic, strong) IBOutlet NSButton *activateAtLaunchButton;
 @property (nonatomic, strong) IBOutlet NSButton *deactivateOnManualSleepButton;
 @property (nonatomic, strong) IBOutlet NSButton *showAtLaunchButton;
+
+@property (nonatomic, strong) IBOutlet NSButton *quitButton;
+@property (nonatomic, strong) IBOutlet NSButton *closeButton;
 
 @end
--- src/Caffeine/Classes/CAPreferencesWindowController.m
+++ src/Caffeine/Classes/CAPreferencesWindowController.m
@@ -9,10 +9,16 @@
 
 @implementation CAPreferencesWindowController
 
+@synthesize iconView;
+@synthesize informationTextField;
+@synthesize instructionsTextField;
+@synthesize durationsTextField;
 @synthesize durationButton;
 @synthesize activateAtLaunchButton;
 @synthesize deactivateOnManualSleepButton;
 @synthesize showAtLaunchButton;
+@synthesize quitButton;
+@synthesize closeButton;
 
 - (id)init
 {
@@ -22,6 +28,88 @@
     }
     
     return self;
+}
+
+- (void)loadWindow
+{
+    [super loadWindow];
+    
+    [durationsTextField sizeToFit];
+    [durationButton sizeToFit];
+    [activateAtLaunchButton sizeToFit];
+    [deactivateOnManualSleepButton sizeToFit];
+    [showAtLaunchButton sizeToFit];
+    [closeButton sizeToFit];
+    [quitButton sizeToFit];
+    
+    CGFloat toolbarHeight = self.window.frame.size.height - self.window.contentLayoutRect.size.height;
+    NSEdgeInsets edgeInsets = NSEdgeInsetsMake(20.0, 20.0, 13.0, 20.0);
+    
+    CGFloat textX = edgeInsets.left + iconView.frame.size.width + edgeInsets.right;
+    CGFloat textWidth = MAX(MAX(activateAtLaunchButton.frame.size.width, deactivateOnManualSleepButton.frame.size.width), showAtLaunchButton.frame.size.width);
+
+    CGFloat windowHeight = 0.0;
+    CGFloat windowWidth = textX + textWidth + edgeInsets.left;
+        
+    windowHeight += edgeInsets.bottom;
+    quitButton.frame = NSMakeRect(13.0,
+                                  windowHeight,
+                                  quitButton.frame.size.width,
+                                  quitButton.frame.size.height);
+    closeButton.frame = NSMakeRect(windowWidth - 13.0 - closeButton.frame.size.width,
+                                   windowHeight,
+                                   closeButton.frame.size.width,
+                                   closeButton.frame.size.height);
+    windowHeight += quitButton.frame.size.height;
+    
+    
+    windowHeight += edgeInsets.bottom;
+    showAtLaunchButton.frame = NSMakeRect(textX,
+                                          windowHeight,
+                                          showAtLaunchButton.frame.size.width,
+                                          showAtLaunchButton.frame.size.height);
+    windowHeight += showAtLaunchButton.frame.size.height + 4.0;
+    deactivateOnManualSleepButton.frame = NSMakeRect(textX,
+                                                     windowHeight,
+                                                     deactivateOnManualSleepButton.frame.size.width,
+                                                     deactivateOnManualSleepButton.frame.size.height);
+    windowHeight += deactivateOnManualSleepButton.frame.size.height + 4.0;
+    activateAtLaunchButton.frame = NSMakeRect(textX,
+                                              windowHeight,
+                                              activateAtLaunchButton.frame.size.width,
+                                              activateAtLaunchButton.frame.size.height);
+    windowHeight += activateAtLaunchButton.frame.size.height + 4.0;
+    durationsTextField.frame = NSMakeRect(textX,
+                                          windowHeight,
+                                          durationsTextField.frame.size.width,
+                                          durationButton.frame.size.height);
+    durationButton.frame = NSMakeRect(textX + durationsTextField.frame.size.width + 4.0,
+                                      windowHeight,
+                                      durationButton.frame.size.width,
+                                      durationButton.frame.size.height);
+    windowHeight += durationButton.frame.size.height;
+    
+    windowHeight += 3*edgeInsets.bottom;
+    NSSize instructionsTextFieldSize = [instructionsTextField sizeThatFits:NSMakeSize(textWidth, CGFLOAT_MAX)];
+    instructionsTextField.frame = NSMakeRect(textX,
+                                             windowHeight,
+                                             textWidth,
+                                             instructionsTextFieldSize.height);
+    windowHeight += instructionsTextField.frame.size.height + edgeInsets.bottom;
+    NSSize informationTextFieldSize = [informationTextField sizeThatFits:NSMakeSize(textWidth, CGFLOAT_MAX)];
+    informationTextField.frame = NSMakeRect(textX,
+                                            windowHeight,
+                                            textWidth,
+                                            informationTextFieldSize.height);
+    windowHeight += informationTextField.frame.size.height;
+    
+    iconView.frame = NSMakeRect(edgeInsets.left,
+                                windowHeight - iconView.frame.size.height,
+                                iconView.frame.size.height,
+                                iconView.frame.size.width);
+    
+    windowHeight += edgeInsets.top + toolbarHeight;
+    [self.window setFrame:NSMakeRect(0.0, 0.0, windowWidth, windowHeight) display:NO];
 }
 
 - (void)windowDidLoad
